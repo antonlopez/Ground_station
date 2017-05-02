058033ee87d47e785df5476929f31f16ea97977b
Juan Lopez <anton.lopez09@gmail.com>
2017-05-01 18:53:08 -0700

    Update pwm.c

--- pwm.c
+++ pwm.c
@@ -34,45 +34,12 @@ void PwmConfig(uint32_t ui32Load, uint16_t Roll, uint16_t Pitch)
 
 
 
-
-
-
-
-
-
-    /*
-	// SET PWM CLOCK
-	SysCtlPWMClockSet(SYSCTL_PWMDIV_64);
-	
-	// SETUP PWM PERIPHERAL
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-	
-
-	// ENABLE PWM PINS ON PERIPHERAL C
-	GPIOPinTypePWM(GPIO_PORTA_BASE, GPIO_PIN_6);
-	GPIOPinConfigure(GPIO_PA6_M1PWM2);
-	//GPIOPinConfigure(GPIO_PD1_M0PWM7);
-	
-	// ENABLE GENERATORS
-	PWMGenConfigure(PWM1_BASE, PWM_GEN_1, PWM_GEN_MODE_DOWN);
-	PWMGenPeriodSet(PWM1_BASE, PWM_GEN_1, ui32Load);
-	
-	// SET PULSEWIDTH
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_2, Roll * ui32Load / 1000);
-	//PWMPulseWidthSet(PWM1_BASE, PWM_OUT_7, Pitch * ui32Load / 1000);
-	
-	// ENABLE OUTPUTS
-	PWMGenEnable(PWM1_BASE, PWM_GEN_1);
-	PWMOutputState(PWM1_BASE, PWM_OUT_2_BIT, true);  */
 	
 }
 
 void PWMDCSet (uint32_t ui32Load, uint16_t Roll, uint16_t Pitch)
 {
-	//PWMPulseWidthSet(PWM1_BASE, PWM_OUT_2, Roll * ui32Load / 1000);
-	//(PWM1_BASE, PWM_OUT_7, Pitch * ui32Load / 1000);
+	
 
     PWMPulseWidthSet(PWM1_BASE, PWM_OUT_2, Roll * ui32Load / 1000);
     PWMPulseWidthSet(PWM1_BASE, PWM_OUT_3, Pitch * ui32Load / 1000);
